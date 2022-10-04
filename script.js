49f0d6b3f118d74a16fb80291da37d3aa7407175
sophiadejong <sophiamarinadejongy@gmail.com>
2022-10-04 14:33:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,4 +19,34 @@ const getData = async () => {
 getData()
 
 
+function getFormValue(ev) {
+	// DISABLE DEFAULT FORM SUBMIT ACTION
+	ev.preventDefault();
+  
+	// PICK THE FORM REFERENCE
+	const form = document.getElementById('form1');
+	
+	// GET ALL ELMENTS LIKE INPUT AND SELECT, YOU CAN ADD MANY MORE
+	const allInputs = form.querySelectorAll('input,select');
+	
+	let formObj = {};
+	
+	// GET THE VALUES OF EACH FIELD
+	allInputs.forEach(e => {
+	  
+	  // ALL KEY DEPENDS ON THE NAME IN THE ELEMENT
+	  const key = e.getAttribute('name');
+	  
+	  if(key){
+		formObj[key] = e.value;
+	  } 
+	  
+	});
+	
+	console.log(formObj);
+  }
+
+
+
+
 
